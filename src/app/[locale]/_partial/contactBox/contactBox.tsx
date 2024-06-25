00141eee4262990dcdc6e479e5c0858de5131db8
aparx <undestroy1310@gmail.com>
2024-06-25 22:42:50 +0200

    chore: added phone and email to contactbox

--- src/app/[locale]/_partial/contactBox/contactBox.tsx
+++ src/app/[locale]/_partial/contactBox/contactBox.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@/components";
+import { Button, TextFont } from "@/components";
 import { useTranslations } from "next-intl";
 import { LuContact } from "react-icons/lu";
+import { MdEmail, MdPhone } from "react-icons/md";
 import { GridBox } from "../../_components";
 import css from "./contactBox.module.css";
 
@@ -9,10 +10,26 @@ export function ContactBox() {
 
   return (
     <GridBox.Root type="stretch" className={css.root}>
-      <GridBox.Header
-        title={t("Contact.title")}
-        subtitle={t("Contact.subtitle")}
-      />
+      <div>
+        <GridBox.Header
+          title={t("Contact.title")}
+          subtitle={t("Contact.subtitle")}
+        />
+        <footer>
+          <ul aria-label="Contact possibilities elsewhere">
+            <TextFont asChild type="mono">
+              <li>
+                <MdPhone />
+                <p>+49 01573 8947352</p>
+              </li>
+            </TextFont>
+            <li>
+              <MdEmail />
+              <p>contact@vizeb.dev</p>
+            </li>
+          </ul>
+        </footer>
+      </div>
       <section>
         <h5>
           <LuContact />
