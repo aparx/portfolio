c7d89f1bc39b8ac1a8769bfe32b46a9dcd374199
aparx <undestroy1310@gmail.com>
2024-06-29 18:44:46 +0200

    fix: memoize in production was in reverse

--- src/utils/memoize.ts
+++ src/utils/memoize.ts
@@ -18,12 +18,12 @@ export function memoizeAsync<T>(getter: () => Promise<T>): () => Promise<T> {
 export function memoizeAsyncProduction<T>(
   fn: () => Promise<T>
 ): () => Promise<T> {
-  if (process.env.NODE_ENV !== "development") return fn;
+  if (process.env.NODE_ENV === "development") return fn;
   return memoizeAsync<T>(fn);
 }
 
 /** Uses `memoizeSync` if not in `development` mode */
 export function memoizeSyncProduction<T>(fn: () => T): () => T {
-  if (process.env.NODE_ENV !== "development") return fn;
+  if (process.env.NODE_ENV === "development") return fn;
   return memoizeSync<T>(fn);
 }
