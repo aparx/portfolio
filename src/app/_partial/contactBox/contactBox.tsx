2ef8643b0dd615c50df676b65254c381a2f3abb6
aparx <undestroy1310@gmail.com>
2024-07-07 04:26:40 +0200

    feat: added floating CTA button that links to contact box

--- src/app/_partial/contactBox/contactBox.tsx
+++ src/app/_partial/contactBox/contactBox.tsx
@@ -3,11 +3,17 @@ import { Button, TextFont } from "@/components";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
-import { ComponentPropsWithoutRef, InputHTMLAttributes, useId } from "react";
+import {
+  ComponentPropsWithoutRef,
+  InputHTMLAttributes,
+  useId,
+  useState,
+} from "react";
 import { useFormState, useFormStatus } from "react-dom";
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
-import { MdEmail, MdPhone } from "react-icons/md";
+import { MdArrowDownward, MdEmail, MdPhone } from "react-icons/md";
 import { SiTypescript } from "react-icons/si";
+import { useInView } from "react-intersection-observer";
 import { GridBox } from "../../_components";
 import { saveContactForm } from "../../actions";
 import { contactFormSchema } from "../../schemas";
@@ -15,48 +21,68 @@ import css from "./contactBox.module.css";
 
 export function ContactBox() {
   const t = useTranslations("index");
+  const [showFloatingButton, setShowFloatingButton] = useState(false);
+
+  const phone = process.env.NEXT_PUBLIC_CONTACT_PHONE;
+  const email = process.env.NEXT_PUBLIC_CONTACT_EMAIL;
+
+  const { ref } = useInView({
+    onChange: (inView) => setShowFloatingButton(!inView),
+  });
 
   return (
-    <GridBox.Root type="stretch" className={css.root}>
-      <div>
-        <GridBox.Header
-          title={t("Contact.title")}
-          subtitle={t("Contact.subtitle")}
-        />
-        <footer>
-          <ul aria-label="Contact possibilities elsewhere">
-            <TextFont asChild type="mono">
-              <li>
-                <Link
-                  href={`tel:${process.env.NEXT_PUBLIC_CONTACT_PHONE!.replaceAll(/ /g, "")}`}
-                >
-                  <MdPhone aria-label="Phone" />
-                  <address typeof="phone">
-                    {process.env.NEXT_PUBLIC_CONTACT_PHONE}
-                  </address>
-                </Link>
-              </li>
-            </TextFont>
-            <li>
-              <Link href={`mailto:${process.env.NEXT_PUBLIC_CONTACT_EMAIL}`}>
-                <MdEmail aria-label="Email" />
-                <address typeof="email">
-                  {process.env.NEXT_PUBLIC_CONTACT_EMAIL}
-                </address>
-              </Link>
-            </li>
-          </ul>
-        </footer>
-      </div>
-      <section>
-        <h5>
-          <VisuallyHidden>Contact Form</VisuallyHidden>
-          <SiTypescript />
-          <span aria-hidden>contactForm.tsx</span>
-        </h5>
-        <Form />
-      </section>
-    </GridBox.Root>
+    <>
+      <GridBox.Root type="stretch" className={css.root}>
+        <div>
+          <GridBox.Header
+            title={t("Contact.title")}
+            subtitle={t("Contact.subtitle")}
+          />
+          <footer>
+            <ul aria-label="Contact possibilities elsewhere">
+              {phone && (
+                <TextFont asChild type="mono">
+                  <li>
+                    <Link href={`tel:${phone.replaceAll(/ /g, "")}`}>
+                      <MdPhone aria-label="Phone" />
+                      <address typeof="phone">{phone}</address>
+                    </Link>
+                  </li>
+                </TextFont>
+              )}
+              {email && (
+                <li>
+                  <Link href={`mailto:${email}`}>
+                    <MdEmail aria-label="Email" />
+                    <address typeof="email">{email}</address>
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </footer>
+        </div>
+        <section ref={ref} id="contact">
+          <h5>
+            <VisuallyHidden>Contact Form</VisuallyHidden>
+            <SiTypescript />
+            <span aria-hidden>contactForm.tsx</span>
+          </h5>
+          <Form />
+        </section>
+      </GridBox.Root>
+      <Button
+        asChild
+        appearance="cta"
+        className={css.floatButton}
+        aria-hidden
+        data-visible={showFloatingButton}
+      >
+        <Link href="#contact" style={{ textDecoration: "none" }}>
+          Get in touch
+          <MdArrowDownward />
+        </Link>
+      </Button>
+    </>
   );
 }
 
