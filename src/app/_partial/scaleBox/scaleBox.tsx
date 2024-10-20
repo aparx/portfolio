bd73b69678ce6792c0d4a236096ba8d9630367fd
aparx <undestroy1310@gmail.com>
2024-10-21 00:59:38 +0200

    feat(scaleBox): replaced basic chart with client / server models

--- src/app/_partial/scaleBox/scaleBox.tsx
+++ src/app/_partial/scaleBox/scaleBox.tsx
@@ -1,12 +1,9 @@
 "use client";
 import { GridBox } from "@/app/_components";
-import { Chart as ChartJS, LineElement } from "chart.js/auto";
 import { useTranslations } from "next-intl";
-import { Line as LineChart } from "react-chartjs-2";
-import { FaArrowTrendUp } from "react-icons/fa6";
+import { FaArrowTrendUp, FaDesktop, FaServer } from "react-icons/fa6";
 import css from "./scaleBox.module.css";
-
-ChartJS.register(LineElement);
+import { MdRouter } from "react-icons/md";
 
 export function ScaleBox() {
   const t = useTranslations("index.Scale");
@@ -19,51 +16,19 @@ export function ScaleBox() {
         icon={<FaArrowTrendUp />}
         intro={t("intro")}
       />
-      <div className={css.scale} style={{ minWidth: 0, minHeight: 0 }}>
-        <LineChart
-          options={{
-            responsive: true,
-            scales: {
-              x: {
-                grid: {
-                  color: "#212121",
-                  drawTicks: false,
-                },
-              },
-              y: {
-                ticks: {
-                  display: false,
-                },
-                grid: {
-                  color: "#212121",
-                },
-              },
-            },
-          }}
-          data={{
-            labels: ["Q1'23", "Q2'23", "Q3'23", "Q4'23", "Q1'24", "Q2'24"],
-            datasets: [
-              {
-                label: t("chart.users"),
-                data: [10, 6421, 34952, 95621, 100000, 134000],
-                borderWidth: 1,
-                borderColor: "#64b5e2",
-                pointBackgroundColor: "black",
-                pointHoverBackgroundColor: "#64b5e2",
-                tension: 0,
-              },
-              {
-                label: t("chart.invocations"),
-                data: [0, 10000, 60347, 134000, 160231, 200562],
-                borderWidth: 1,
-                borderColor: "rgba(210, 210, 210)",
-                pointHoverBackgroundColor: "rgba(210, 210, 210)",
-                pointBackgroundColor: "black",
-                tension: 0,
-              },
-            ],
-          }}
-        />
+      <div className={css.container} style={{ minWidth: 0, minHeight: 0 }}>
+        <div className={css.clientContainer}>
+          {Array.from({ length: 25 }, (_, i) => (
+            <div key={i} className={css.client}>
+              <FaDesktop style={{ fontSize: "1.3em" }} />
+            </div>
+          ))}
+        </div>
+        <div className={css.serverContainer}>
+          <div className={css.server}>
+            <MdRouter style={{ fontSize: "1.5em" }} />
+          </div>
+        </div>
       </div>
     </GridBox.Root>
   );
