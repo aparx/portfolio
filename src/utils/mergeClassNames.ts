1a2ae36d7a5f28f423f91b3255ec545491a5c478
aparx <undestroy1310@gmail.com>
2024-10-21 20:38:50 +0200

    chore: improved SEO & updated colors

--- src/utils/mergeClassNames.ts
+++ src/utils/mergeClassNames.ts
@@ -1,6 +1,7 @@
 import { Primitive } from "./types";
 
 export function mergeClassNames(...args: Primitive[]): string | undefined {
+  // Note: we accept primitives for class names that depend on truthy conditions
   const strings = args.filter((x) => typeof x === "string");
   return strings.length !== 0 ? strings.join(" ") : undefined;
 }
